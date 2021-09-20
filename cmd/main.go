b7b1a2ecab0df58cd264122f9e78ad8cd0f00f36
jodi <chuthiminhhang_t62@hus.edu.vn>
2021-09-20 22:38:15 +0700

    Add product repo

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,32 @@
 package main
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/je117er/ocg-final-project/internal/product/repository"
+	"log"
+)
+
+var (
+	ctx context.Context
+)
+
+func main() {
+	db, err := sql.Open("mysql", "admin:123456Aa@tcp(127.0.0.1:3306)/vaccine-covid-19")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+	fmt.Println("you")
+	productRepo := repository.NewProductRepository(db)
+	fmt.Println("you two")
+	products, err := productRepo.All(ctx)
+	fmt.Println("you three")
+	//product, err := productRepo.ByID(ctx, "c370c173-19e2-11ec-a931-0242ac170002")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%+v\n", products)
+}
---
+++ internal/product/repository/productRepository.go
@@ -0,0 +1,117 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/je117er/ocg-final-project/internal/models"
+)
+
+type ProductRepository struct {
+	Conn *sql.DB
+}
+
+func NewProductRepository(Conn *sql.DB) *ProductRepository {
+	return &ProductRepository{Conn}
+}
+
+func (p *ProductRepository) ByID(ctx context.Context, id string) (*models.Product, error) {
+	var product *models.Product
+	query := `SELECT BIN_TO_UUID(id) id, name, price, vendor, vaccine_type, authorized_ages, dose, antigen_nature, route_of_administration, storage_requirements, available_formats, diluent, adjuvant, alternate_name, minimum_interval, immunization_schedule, authorized_interval, extended_interval, background, regulatory_actions, safety_status, authorization_status, trials, distribution, funding, slug, image, lot_number, expiry_date FROM product WHERE id = ? LIMIT 1`
+	err := p.Conn.QueryRowContext(ctx, query, id).Scan(
+		&product.ID,
+		&product.Name,
+		&product.Price,
+		&product.Vendor,
+		&product.VaccineType,
+		&product.AuthorizedAges,
+		&product.Dose,
+		&product.AntigenNature,
+		&product.RouteOfAdministration,
+		&product.StorageRequirements,
+		&product.AvailableFormats,
+		&product.Diluent,
+		&product.Adjuvant,
+		&product.AlternateName,
+		&product.MinimumInterval,
+		&product.ImmunizationSchedule,
+		&product.AuthorizedInterval,
+		&product.ExtendedInterval,
+		&product.Background,
+		&product.RegulatoryActions,
+		&product.SafetyStatus,
+		&product.AuthorizationStatus,
+		&product.Trials,
+		&product.Distribution,
+		&product.Funding,
+		&product.Slug,
+		&product.Image,
+		&product.LotNumber,
+		&product.ExpiryDate,
+	)
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, fmt.Errorf("No product with id %s\n", id)
+	case err != nil:
+		return nil, fmt.Errorf("Query error: %v\n", err)
+	}
+	return product, nil
+}
+
+func (p *ProductRepository) All(ctx context.Context) ([]*models.Product, error) {
+	var products []*models.Product
+	query := `SELECT BIN_TO_UUID(id) id, name, price, vendor, vaccine_type, authorized_ages, dose, antigen_nature, route_of_administration, storage_requirements, available_formats, diluent, adjuvant, alternate_name, minimum_interval, immunization_schedule, authorized_interval, extended_interval, background, regulatory_actions, safety_status, authorization_status, trials, distribution, funding, slug, image, lot_number, expiry_date FROM product`
+	fmt.Println("you are hereee")
+	rows, err := p.Conn.QueryContext(ctx, query)
+	fmt.Println("you are hereee")
+	if err != nil {
+		return nil, nil
+	}
+	defer rows.Close()
+	var product *models.Product
+	for rows.Next() {
+		if err := rows.Scan(
+			&product.ID,
+			&product.Name,
+			&product.Price,
+			&product.Vendor,
+			&product.VaccineType,
+			&product.AuthorizedAges,
+			&product.Dose,
+			&product.AntigenNature,
+			&product.RouteOfAdministration,
+			&product.StorageRequirements,
+			&product.AvailableFormats,
+			&product.Diluent,
+			&product.Adjuvant,
+			&product.AlternateName,
+			&product.MinimumInterval,
+			&product.ImmunizationSchedule,
+			&product.AuthorizedInterval,
+			&product.ExtendedInterval,
+			&product.Background,
+			&product.RegulatoryActions,
+			&product.SafetyStatus,
+			&product.AuthorizationStatus,
+			&product.Trials,
+			&product.Distribution,
+			&product.Funding,
+			&product.Slug,
+			&product.Image,
+			&product.LotNumber,
+			&product.ExpiryDate,
+		); err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+	rerr := rows.Close()
+	if rerr != nil {
+		return nil, rerr
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return products, nil
+
+}
