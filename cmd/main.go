c8f7527b6ba692277777918d5cc6184d26a6ed4b
jodi <chuthiminhhang_t62@hus.edu.vn>
2021-09-21 14:10:23 +0700

    Move testing code to server

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
-	"github.com/je117er/ocg-final-project/internal/product/controllers"
-	"github.com/je117er/ocg-final-project/internal/product/repository"
-	"github.com/je117er/ocg-final-project/internal/product/services"
+	"github.com/je117er/ocg-final-project/internal/server"
 	"github.com/je117er/ocg-final-project/internal/utils"
 	"log"
-	"net/http"
-	"time"
 )
 
 func main() {
@@ -21,23 +13,5 @@ func main() {
 	sugarLogger.Debug("Test log debug")
 	sugarLogger.Error("Test log err")
 
-	DB, err := sql.Open("mysql", "root:123456Aa@@tcp(localhost:6033)/vaccine-covid-19?charset=utf8mb4&parseTime=True&loc=Local")
-	//DB, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", "root", "123456Aa@", "localhost", "6033", "vaccine-covid-19" ))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer DB.Close()
-	fmt.Println("you")
-	sugarLogger.Error("Test log err")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	productRepo := repository.NewProductRepository(DB)
-	serviceRepo := services.NewProductService(productRepo)
-
-	r := mux.NewRouter()
-	controllers.NewProductController(serviceRepo, ctx, r)
-
-	sugarLogger.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(server.InitServer())
 }
--- internal/utils/responseHelper.go
+++ internal/utils/responseHelper.go
@@ -5,8 +5,9 @@ import (
 	"net/http"
 )
 
-func ResponseWithJson(writer http.ResponseWriter, status int, object interface{})  {
+func ResponseWithJson(writer http.ResponseWriter, status int, object interface{}) {
 	writer.Header().Set("Content-Type", "application/json")
+	//writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
 	writer.WriteHeader(status)
 	if err := json.NewEncoder(writer).Encode(object); err != nil {
 		return
