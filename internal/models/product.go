64cc98fc8480b8b734ca17048b2ec64c1482024d
jodi <chuthiminhhang_t62@hus.edu.vn>
2021-09-21 14:48:22 +0700

    Add model-to-response converter in product api

--- internal/models/product.go
+++ internal/models/product.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"time"
 )
 
 type Product struct {
@@ -36,4 +37,68 @@ type Product struct {
 	ExpiryDate            sql.NullTime
 }
 
+type ProductResponse struct {
+	ID                    string
+	Name                  string
+	Price                 string
+	Vendor                string
+	VaccineType           string
+	AuthorizedAges        int32
+	Dose                  string
+	AntigenNature         string
+	RouteOfAdministration string
+	StorageRequirements   string
+	AvailableFormats      string
+	Diluent               string
+	Adjuvant              string
+	AlternateName         string
+	MinimumInterval       int32
+	ImmunizationSchedule  int32
+	AuthorizedInterval    int32
+	ExtendedInterval      int32
+	Background            string
+	RegulatoryActions     string
+	SafetyStatus          string
+	AuthorizationStatus   bool
+	Trials                string
+	Distribution          string
+	Funding               string
+	Slug                  string
+	Image                 string
+	LotNumber             string
+	ExpiryDate            time.Time
+}
 
+func (p Product) ModelToResponse() *ProductResponse {
+	return &ProductResponse{
+		ID:                    p.ID,
+		Name:                  p.Name.String,
+		Price:                 p.Price.String,
+		Vendor:                p.Vendor.String,
+		VaccineType:           p.VaccineType.String,
+		AuthorizedAges:        p.AuthorizedAges.Int32,
+		Dose:                  p.Dose.String,
+		AntigenNature:         p.AntigenNature.String,
+		RouteOfAdministration: p.RouteOfAdministration.String,
+		StorageRequirements:   p.StorageRequirements.String,
+		AvailableFormats:      p.AvailableFormats.String,
+		Diluent:               p.Diluent.String,
+		Adjuvant:              p.Adjuvant.String,
+		AlternateName:         p.AlternateName.String,
+		MinimumInterval:       p.MinimumInterval.Int32,
+		ImmunizationSchedule:  p.ImmunizationSchedule.Int32,
+		AuthorizedInterval:    p.AuthorizedInterval.Int32,
+		ExtendedInterval:      p.ExtendedInterval.Int32,
+		Background:            p.Background.String,
+		RegulatoryActions:     p.RegulatoryActions.String,
+		SafetyStatus:          p.SafetyStatus.String,
+		AuthorizationStatus:   p.AuthorizationStatus.Bool,
+		Trials:                p.Trials.String,
+		Distribution:          p.Distribution.String,
+		Funding:               p.Funding.String,
+		Slug:                  p.Slug.String,
+		Image:                 p.Image.String,
+		LotNumber:             p.LotNumber.String,
+		ExpiryDate:            p.ExpiryDate.Time,
+	}
+}
