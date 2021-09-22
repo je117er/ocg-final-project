e08cba71506f8ce8147a7af25f6f5de1fdcc01d8
thanbv1510 <thanbv1510@gmail.com>
2021-09-22 15:19:14 +0700

    fix null data

--- web/vaccine-service/src/views/ProductDetail.vue
+++ web/vaccine-service/src/views/ProductDetail.vue
@@ -1,46 +1,48 @@
 <template>
   <div>
     <div class="container is-fullhd">
-      <h1 v-if="loading">Loading...</h1>
-      <div v-else class="notification">
-        <div class="tile is-ancestor">
-          <div class="tile is-vertical">
-            <div class="tile">
+      <template v-if="!loading">Loading...</template>
+      <template v-else>
+        <div class="notification">
+          <div class="tile is-ancestor">
+            <div class="tile is-vertical">
+              <div class="tile">
+                <div class="tile is-parent">
+                  <article class="tile is-child notification is-info">
+                    <figure class="image is-4by3">
+                      <img :src="require('@/assets/images/logo.svg')" alt="logo.svg">
+                    </figure>
+                  </article>
+                </div>
+              </div>
               <div class="tile is-parent">
-                <article class="tile is-child notification is-info">
-                  <figure class="image is-4by3">
-                    <img :src="require('@/assets/images/logo.svg')" alt="logo.svg">
-                  </figure>
+                <article class="tile is-child notification is-danger">
+                  <p class="title">{{ productDetail.Slug }}</p>
+                  <p class="subtitle">{{ productDetail.Name }}</p>
+                  <div class="content has-text-left">
+                    <p><strong>Price:</strong> {{ productDetail.Price }} $</p>
+                    <p><strong>Vender: </strong> {{ productDetail.Vendor }}</p>
+                    <p><strong>LotNumber: </strong>{{ productDetail.LotNumber }}</p>
+                    <p><strong>ExpiryDate: </strong>{{ productDetail.ExpiryDate }}</p>
+                  </div>
                 </article>
               </div>
             </div>
-            <div class="tile is-parent">
-              <article class="tile is-child notification is-danger">
-                <p class="title">{{ productDetail.Slug }}</p>
-                <p class="subtitle">{{ productDetail.Name }}</p>
-                <div class="content has-text-left">
-                  <p><strong>Price:</strong> {{ productDetail.Price }} $</p>
-                  <p><strong>Vender: </strong> {{ productDetail.Vendor }}</p>
-                  <p><strong>LotNumber: </strong>{{ productDetail.LotNumber }}</p>
-                  <p><strong>ExpiryDate: </strong>{{ productDetail.ExpiryDate }}</p>
+            <div class="tile is-parent  is-8">
+              <article class="tile is-child notification has-text-left">
+                <div class="content">
+                  <div class="content">
+                    <p><strong>VaccineType: </strong>{{ productDetail.VaccineType }}</p>
+                    <p><strong>Background: </strong>{{ productDetail.Background }}</p>
+                    <p><strong>RegulatoryActions: </strong>{{ productDetail.RegulatoryActions }}</p>
+                    <p><strong>SafetyStatus: </strong>{{ productDetail.SafetyStatus }}</p>
+                  </div>
                 </div>
               </article>
             </div>
           </div>
-          <div class="tile is-parent  is-8">
-            <article class="tile is-child notification has-text-left">
-              <div class="content">
-                <div class="content">
-                  <p><strong>VaccineType: </strong>{{ productDetail.VaccineType }}</p>
-                  <p><strong>Background: </strong>{{ productDetail.Background }}</p>
-                  <p><strong>RegulatoryActions: </strong>{{ productDetail.RegulatoryActions }}</p>
-                  <p><strong>SafetyStatus: </strong>{{ productDetail.SafetyStatus }}</p>
-                </div>
-              </div>
-            </article>
-          </div>
         </div>
-      </div>
+      </template>
     </div>
   </div>
 </template>
@@ -52,13 +54,11 @@ export default {
   name: 'ProductDetail.vue',
   data() {
     return {
-      productDetail: {},
+      productDetail: null,
       loading: false,
-      // deep: true,
     };
   },
   created() {
-    this.loading = true;
     this.fetchData();
   },
   watch: {
@@ -67,15 +67,13 @@ export default {
   methods: {
     async fetchData() {
       const fetchedID = this.$route.params.id;
-      this.productDetail = null;
       try {
         const response = await Vue.axios.get(`http://localhost:8088/product/${this.$route.params.id}`);
         if (this.$route.params.id !== fetchedID) return;
-        console.log(response);
+
         this.productDetail = response.data;
-        this.loading = false;
+        this.loading = true;
       } catch (e) {
-        console.log(e);
         this.loading = false;
       }
     },
