9d47b47bc659c03bafad36431d3089cb3db383d5
thanbv1510 <thanbv1510@gmail.com>
2021-09-26 01:13:23 +0700

    update query and set time = 10s for every scan emails

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"database/sql"
 	customerRepository "github.com/je117er/ocg-final-project/internal/customer/repository"
-	"github.com/je117er/ocg-final-project/internal/rabbitmq"
+	"github.com/je117er/ocg-final-project/internal/models"
 	"github.com/je117er/ocg-final-project/internal/utils"
 	"github.com/robfig/cron/v3"
 )
@@ -13,32 +13,31 @@ type Scheduler struct {
 	db       *sql.DB
 	cron     *cron.Cron
 	ctx      context.Context
-	producer *rabbitmq.Producer
+	dataChan chan<- *models.SentMail
 }
 
 var logger = utils.SugarLog()
 
-func NewScheduler(ctx context.Context, db *sql.DB, producer *rabbitmq.Producer) *Scheduler {
+func NewScheduler(ctx context.Context, db *sql.DB, dataChan chan<- *models.SentMail) *Scheduler {
 	return &Scheduler{
 		db:       db,
 		cron:     cron.New(cron.WithSeconds()),
 		ctx:      ctx,
-		producer: producer,
+		dataChan: dataChan,
 	}
 }
 
 func (scheduler *Scheduler) Start() {
 	logger.Info("Start scheduler...")
-	scheduler.cron.AddFunc("10 * * * * *", scheduler.schedulerJob)
-	scheduler.producer.Start()
+	scheduler.cron.AddFunc("*/10 * * * * *", scheduler.schedulerJob)
 	scheduler.cron.Start()
 }
 
 func (scheduler *Scheduler) schedulerJob() {
 	logger.Info("Scan to email....")
 	customerRepo := customerRepository.NewCustomerRepository(scheduler.db)
-	emails, _ := customerRepo.GetUnSendEmails(scheduler.ctx, 5, 10)
+	emails, _ := customerRepo.GetUnSendEmails(scheduler.ctx, 10)
 	for _, v := range emails {
-		scheduler.producer.Public(v)
+		scheduler.dataChan <- v
 	}
 }
